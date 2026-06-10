eb247739d6369fa33f8ddfedaebf2793a9e03ddc
Vinod Guptha <Vinodhkumar0339@gmail.com>
2026-06-10 13:31:51 +0530

    Demo specific file commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //Testing with my own knowledge how to commit
+//Finally i understand how to commit and push the code form powershell - just we have to that particular path and use below commands 
+//git add .   -  if you want all files then use dot if you want specific file then mention git add program.cs (file name)
+//git commit -m "comments inside of this"
+//git push
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
